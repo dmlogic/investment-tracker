0714d16633233384ef47d6c23359ebe6db281dd5
Darren Miller <darren@dmlogic.com>
2016-01-26 14:53:01 +0000

    readme

--- config/funds.sample.php
+++ config/funds.sample.php
@@ -5,27 +5,28 @@ return  [
         'group_name' => 'Pension holdings',
         'funds' => [
             [
-                'name'       => 'Google',
-                'type'       => 'share',
-                'cost'       => 100,
-                'units_held' => 1,
-                'provider'   => 'hl',
-                'url'        => 'http://www.hl.co.uk/shares/shares-search-results/a/alphabet-inc-npv-c/share-charts'
+                'name'            => 'Google',
+                'type'            => 'share',
+                'cost'            => 100,
+                'units_held'      => 1,
+                'provider'        => 'hl',
+                'url'             => 'http://www.hl.co.uk/shares/shares-search-results/a/alphabet-inc-npv-c/share-charts',
+                'lookup_currency' => 'USD',
             ]
         ]
     ],
     [
         'group_name' => 'ISA funds',
         'funds' => [
-
             [
-                'name'       => 'Standard Life UK Equity Unconstrained Institutional Class - Accumulation (GBP)',
-                'type'       => 'fund',
-                'cost'       => 100,
-                'units_held' => 1,
-                'provider'   => 'hl',
-                'xml_code'   => 'FTP12',
-                'url'        => 'http://www.hl.co.uk/funds/fund-discounts,-prices--and--factsheets/search-results/s/standard-life-inv-uk-equity-unconstrained-inst-accumulation/charts'
+                'name'            => 'Standard Life UK Equity Unconstrained Institutional Class - Accumulation (GBP)',
+                'type'            => 'fund',
+                'cost'            => 100,
+                'units_held'      => 1,
+                'provider'        => 'hl',
+                'xml_code'        => 'FTP12',
+                'url'             => 'http://www.hl.co.uk/funds/fund-discounts,-prices--and--factsheets/search-results/s/standard-life-inv-uk-equity-unconstrained-inst-accumulation/charts',
+                'lookup_currency' => 'pence', // use 'pence' for any UK fund
             ],
         ]
     ]
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Investment tracker
+
+Originally made to view an overall summary of ISA funds held at [Hargreaves Landsdown](https://hl.co.uk), it can be used for anyone with a suitable [Provider](https://github.com/dmlogic/investment-tracker/blob/master/app/Providers/Factory.php#L10) class.
+
+## Installation
+
+* Run `composer install`
+* Copy `config/funds.sample.php` to `config/funds.php` and complete your data
+* Fire up a webserver from the `public` folder and visit with your browser
+
+## Currency issues
+
+As this was made for HL, most pricing is looked up in UK pence which should have the label 'pence' in your config file. Conversion of investment totals should work fine and is done via [fixer.io](http://fixer.io/)
