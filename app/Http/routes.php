a55e27f591874abb6877eee785ce149a72506f7f
Darren Miller <darren@dmlogic.com>
2016-09-27 15:21:04 +0100

    Clear out for v2

--- app/Http/routes.php
+++
@@ -1,5 +0,0 @@
-<?php
-Route::group(['middleware' => ['auth.basic']], function () {
-    Route::get( '/',                           ['as' => 'home', 'uses' => 'Controller@homepage'] );
-    Route::get( '/group/{group}/fund/{fund}',  ['as' => 'fund', 'uses' => 'Controller@fund'] );
-});
