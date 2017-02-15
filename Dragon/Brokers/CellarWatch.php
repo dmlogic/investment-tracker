243fedfc8a878a4c36f64f235f84c6e322a0d623
Darren Miller <darren@dmlogic.com>
2017-02-15 16:33:33 +0000

    Change percentage correction. Live endpoint

--- Dragon/Brokers/CellarWatch.php
+++ Dragon/Brokers/CellarWatch.php
@@ -67,12 +67,8 @@ class CellarWatch extends Broker {
                 'series'   => '0|'.$this->investment->apiUrl.'|'.$this->investment->apiId,
             ]);
         // exit(static::API_ENDPOINT.'?'.$query);
-        // $response = $this->connector->get(static::API_ENDPOINT.'?'.$query,$options);
-        // $this->parseResponse($response->getBody()->__toString());
-
-
-        $json = '[{"color":"#null","name":"Calon Segur, 1996","data":[{"value":695.0,"date":1455235200000},{"value":695.0,"date":1455840000000},{"value":740.0,"date":1456444800000},{"value":725.0,"date":1457049600000},{"value":720.0,"date":1457654400000},{"value":685.0,"date":1458259200000},{"value":720.0,"date":1458864000000},{"value":720.0,"date":1459465200000},{"value":720.0,"date":1460070000000},{"value":720.0,"date":1460674800000},{"value":720.0,"date":1461279600000},{"value":720.0,"date":1461884400000},{"value":720.0,"date":1462489200000},{"value":720.0,"date":1463094000000},{"value":702.0,"date":1463698800000},{"value":700.0,"date":1464303600000},{"value":700.0,"date":1464908400000},{"value":720.0,"date":1465513200000},{"value":720.0,"date":1466118000000},{"value":720.0,"date":1466722800000},{"value":720.0,"date":1467327600000},{"value":720.0,"date":1467932400000},{"value":750.0,"date":1468537200000},{"value":750.0,"date":1469142000000},{"value":750.0,"date":1469746800000},{"value":750.0,"date":1470351600000},{"value":700.0,"date":1470956400000},{"value":750.0,"date":1471561200000},{"value":750.0,"date":1472166000000},{"value":750.0,"date":1472770800000},{"value":750.0,"date":1473375600000},{"value":750.0,"date":1473980400000},{"value":750.0,"date":1474585200000},{"value":750.0,"date":1475190000000},{"value":750.0,"date":1475794800000},{"value":750.0,"date":1476399600000},{"value":750.0,"date":1477004400000},{"value":750.0,"date":1477609200000},{"value":780.0,"date":1478217600000},{"value":780.0,"date":1478822400000},{"value":800.0,"date":1479427200000},{"value":800.0,"date":1480032000000},{"value":800.0,"date":1480636800000},{"value":800.0,"date":1481241600000},{"value":776.0,"date":1481846400000},{"value":788.0,"date":1482451200000},{"value":792.0,"date":1483056000000},{"value":795.0,"date":1483660800000},{"value":795.0,"date":1484265600000},{"value":795.0,"date":1484870400000},{"value":785.0,"date":1485475200000},{"value":785.0,"date":1486080000000},{"value":786.0,"date":1486684800000}]}]';
-        $this->parseResponse($json);
+        $response = $this->connector->get(static::API_ENDPOINT.'?'.$query,$options);
+        $this->parseResponse($response->getBody()->__toString());
     }
 
     /**
@@ -98,7 +94,7 @@ class CellarWatch extends Broker {
 
         $this->liveData->sell_price = $today->value;
         $this->liveData->last_price = $today->value;
-        $this->liveData->last_change  = $today->value - $previous->value;
+        $this->liveData->last_change  = number_format($this->percentageValue($today->value, $previous->value) ,2);
         $changeDirection = '';
         if($today->value > $previous->value) {
             $changeDirection = 'up';
